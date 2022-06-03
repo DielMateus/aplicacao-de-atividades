9273537a6030a730d1758a05d4891a6c9577620b
Diel Mateus <dielmp91@gmail.com>
2022-06-03 17:35:24 -0300

    finalizado aplicacao e testes

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -12,24 +12,45 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState(''); /*Como ele já está vazio, se eu colocar diferente de !=='' vai se verdadeiro*/
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if(!newTaskTitle)return;
+
+    /*Criar uma nova task*/
+    const tasks ={
+      id: Math.random(), /*Math.random gera um ID aleatório */
+      title: newTaskTitle, /*newTaskTitle por que ele já está sendo setado no html e na interface o tipo string(text no-->html) */
+      isComplete: false
+    }
+
+    setTasks(antigoState => [...antigoState, tasks]);
+    setNewTaskTitle('');/*Após inserir a nova task reseta/limpa o imput para inserir uma nova */
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const currentTasks = tasks.map(tasks => tasks.id === id ? {
+      ...tasks, /*spred da task ou seja pegas todos os valores antigos dela  */
+      isComplete: !tasks.isComplete
+    } : tasks);
+
+    setTasks(currentTasks) 
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+     const filteredTasks = tasks.filter(tasks => tasks.id !== id);
+     setTasks(filteredTasks)
   }
 
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>Minhas Tarefas</h2>
 
         <div className="input-group">
           <input 
